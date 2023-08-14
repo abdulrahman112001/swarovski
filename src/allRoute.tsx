8e139d9c6d91d38c9a053212c1b2a63cb913727d
abdulRahman elshikh <abdulrahmanelshikh2001@gmail.com>
2023-08-14 23:30:38 +0300

    s

---
+++ src/allRoute.tsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom"
+
+import RootLayout from "./components/auth-unauth-routes/RootLayout"
+export const AllRoutesProvider = () => {
+    return (
+        <Routes>
+          <Route path="/" element={<RootLayout />} >
+            
+        
+  
+          </Route>
+    
+
+        </Routes>
+    )
+  }
