ef525f3183de466fd29c87f7d8c3a644a0e3184e
abdelrahman mohamed <a.elmetwally10@gmail.com>
2023-08-22 03:11:40 +0300

    create dropdown nav && border nav when scrolling

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
 import {
   createStyles,
   Header,
@@ -21,6 +22,7 @@ import HeartSVG from '../atoms/icons/Heart';
 import PersonSVG from '../atoms/icons/Person';
 import ShopSVG from '../atoms/icons/Shop';
 import { IconSearch } from '@tabler/icons-react';
+import DropDownNavBar from '../organisms/dropDownNavBar';
 
 const useStyles = createStyles((theme) => ({
   inner: {
@@ -72,9 +74,35 @@ interface HeaderSearchProps {
   }[];
 }
 
+// function Check the scrolling position
+const setScrollingAttribute = () => {
+  const scrolling = window.scrollY >= 10;
+
+  if (scrolling) {
+    document.body.setAttribute('scrolling', 'true');
+  } else {
+    document.body.removeAttribute('scrolling');
+  }
+};
+
 const NavbarSection = ({ links }: HeaderSearchProps) => {
   const [opened, { toggle }] = useDisclosure(false);
   const { classes } = useStyles();
+
+  // Check the scrolling position when entering the page
+  useEffect(() => {
+    setScrollingAttribute();
+
+    const handleScroll = () => {
+      setScrollingAttribute();
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   const items = links.map((link) => {
     const menuItems = link.links?.map((item) => (
@@ -88,6 +116,8 @@ const NavbarSection = ({ links }: HeaderSearchProps) => {
           trigger='hover'
           transitionProps={{ exitDuration: 0 }}
           withinPortal
+          openDelay={200}
+          closeDelay={230}
         >
           <Menu.Target>
             <a
@@ -101,7 +131,9 @@ const NavbarSection = ({ links }: HeaderSearchProps) => {
               </Center>
             </a>
           </Menu.Target>
-          <Menu.Dropdown>{menuItems}</Menu.Dropdown>
+          <Menu.Dropdown>
+            <DropDownNavBar />
+          </Menu.Dropdown>
         </Menu>
       );
     }
---
+++ src/components/organisms/dropDownNavBar.tsx
@@ -0,0 +1,123 @@
+import ImageProduct from '../atoms/image/datatest.webp';
+const DropDownNavBar = () => {
+  return (
+    <>
+      <div className='container mx-auto px-7 mt-4 mb-5'>
+        <div className='grid grid-cols-4 py-[1.875rem]'>
+          <div className='col-span-1'>
+            <h2 className='text-[#222] uppercase mb-[15px]'>Bags</h2>
+            <ul className='grid gap-[8px]'>
+              <li>
+                <a href='#' className='hover:underline'>
+                  All bags
+                </a>
+              </li>
+              <li>
+                <a href='#' className='hover:underline'>
+                  Backpacks
+                </a>
+              </li>
+              <li>
+                <a href='#' className='hover:underline'>
+                  Beach bags
+                </a>
+              </li>
+              <li>
+                <a href='#' className='hover:underline'>
+                  Bucket bags
+                </a>
+              </li>
+              <li>
+                <a href='#' className='hover:underline'>
+                  Clutch bags
+                </a>
+              </li>
+              <li>
+                <a href='#' className='hover:underline'>
+                  Mini bags
+                </a>
+              </li>
+              <li>
+                <a href='#' className='hover:underline'>
+                  Cross-body bags
+                </a>
+              </li>
+              <li>
+                <a href='#' className='hover:underline'>
+                  Shoulder bags
+                </a>
+              </li>
+              <li>
+                <a href='#' className='hover:underline'>
+                  Tote bags
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className='col-span-1'>
+            <h2 className='text-[#222] uppercase mb-[15px]'>Discover</h2>
+            <ul className='grid gap-[8px]'>
+              <li>
+                <a href='#' className='hover:underline'>
+                  New in bags
+                </a>
+              </li>
+              <li>
+                <a href='#' className='hover:underline'>
+                  Top 100 iconic bags
+                </a>
+              </li>
+              <li>
+                <a href='#' className='hover:underline'>
+                  Pre-owned bags
+                </a>
+              </li>
+              <li>
+                <a href='#' className='hover:underline'>
+                  Conscious bags
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className='col-span-1'>
+            <h2 className='text-[#222] uppercase mb-[15px]'>
+              Our Sustainable Services
+            </h2>
+            <ul className='grid gap-[8px]'>
+              <li>
+                <a href='#' className='hover:underline'>
+                  Sell designer bags
+                </a>
+              </li>
+              <li>
+                <a href='#' className='hover:underline'>
+                  Sustainability at FARFETCH
+                </a>
+              </li>
+            </ul>
+          </div>
+
+          <div className='flex flex-col gap-[1.9rem] col-span-1'>
+            <div>
+              <img src={ImageProduct} alt='product' />
+            </div>
+
+            <div>
+              <h2 className='text-[#878787] mb-[15px]'>Spotlight on</h2>
+
+              <p className='mb-3'>All bags</p>
+
+              <a href='#' className='underline  hover:text-[#878787]'>
+                Contact us
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DropDownNavBar;
