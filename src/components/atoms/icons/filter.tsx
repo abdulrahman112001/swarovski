3e8b77f6f5e1b210215a6a9f5305c5370ed305eb
abdelrahman mohamed <a.elmetwally10@gmail.com>
2023-09-16 19:39:17 +0300

    create product page and filter

---
+++ src/components/atoms/icons/filter.tsx
@@ -0,0 +1,14 @@
+const Filter_IC = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    viewBox='0 0 24 24'
+    width={24}
+    height={24}
+  >
+    <path
+      fillRule='evenodd'
+      d='M15 19v2H9v-2h6zm3-8v2H6v-2h12zm3-8v2H3V3h18z'
+    />
+  </svg>
+);
+export default Filter_IC;
---
+++ src/components/sections/profile/ProfileSettings.tsx
@@ -0,0 +1,14 @@
+const ProfileSettings = () => {
+  return (
+    <>
+      <div className='h-[1200px] container px-4 py-8 mx-auto'>
+        <div className='grid grid-cols-12 h-full'>
+          <div className='col-span-2 bg-slate-500'>side</div>
+          <div className='col-span-10 bg-slate-950 h-max'>main</div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProfileSettings;
